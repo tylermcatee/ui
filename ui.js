8b8e2bd58a609285762dc0f9582c7b73e5026267
Tyler McAtee <tylermcatee@apple.com>
2016-06-28 23:43:50 -0700

    label layout help #8

--- ui.js
+++ ui.js
@@ -74,8 +74,9 @@ locationInView(view){return{x:this.lastEvent.x,y:this.lastEvent.y};}}
 class ImageView extends View{static imageViewWithFrame(x,y,width,height){var newImageView=new ImageView();newImageView.init();newImageView.setX(x);newImageView.setY(y);newImageView.setWidth(width);newImageView.setHeight(height);return newImageView;}
 init(){this.imageView=document.createElement('img');this.imageView.style.width="100%";this.imageView.style.height="100%";this.imageView.style.position='relative';super.init();this.view.appendChild(this.imageView);}
 setImage(src){this.imageView.src=src;}}
-class Label extends View{static labelWithFrame(x,y,width,height){var newLabel=new Label();newLabel.init();newLabel.setX(x);newLabel.setY(y);newLabel.setWidth(width);newLabel.setHeight(height);return newLabel}
+class Label extends View{static labelWithFrame(x,y,width,height){var newLabel=new Label();newLabel.init();newLabel.setX(x);newLabel.setY(y);newLabel.setWidth(width);newLabel.setHeight(height);newLabel.text='';newLabel.fontSize=16.0;newLabel.fontColor='black';newLabel.textAlign='left';return newLabel;}
 init(){super.init();this.view.id='label';}
+static textSize(text,fontSize){var testDummy=textSize.testDummy||function(){textSize.testDummy=document.createElement("div");textSize.testDummy.style.position='absolute';textSize.testDummy.style.top=-1000;textSize.testDummy.style.left=-1000;textSize.testDummy.style.height='auto';textSize.testDummy.style.width='auto';textSize.testDummy.style.whiteSpace='nowrap';document.body.appendChild(textSize.testDummy);return textSize.testDummy;}();testDummy.innerHTML=text;testDummy.style.fontSize=fontSize;var dummyWidth=(testDummy.clientWidth+1);var dummyHeight=(testDummy.clientHeight+1);return{'width':dummyWidth,'height':dummyHeight};}
 setText(text){this.text=text;this.view.innerHTML=text;}
 setFontSize(fontSize){this.fontSize=fontSize;this.view.style.fontSize=fontSize;}
 setFontColor(fontColor){this.fontColor=fontColor;this.view.style.color=fontColor;}
--- views/label.js
+++ views/label.js
@@ -6,12 +6,35 @@ class Label extends View {
 		newLabel.setY(y);
 		newLabel.setWidth(width);
 		newLabel.setHeight(height);
-		return newLabel
+		newLabel.text = '';
+		newLabel.fontSize = 16.0;
+		newLabel.fontColor = 'black';
+		newLabel.textAlign = 'left';
+		return newLabel;
 	}
 
 	init() {
 		super.init();
 		this.view.id = 'label';
+	}
+
+	static textSize(text, fontSize) {
+		var testDummy = textSize.testDummy || function() {
+			textSize.testDummy = document.createElement("div");
+			textSize.testDummy.style.position = 'absolute';
+			textSize.testDummy.style.top = -1000;
+			textSize.testDummy.style.left = -1000;
+			textSize.testDummy.style.height = 'auto';
+			textSize.testDummy.style.width = 'auto';
+			textSize.testDummy.style.whiteSpace = 'nowrap';
+			document.body.appendChild(textSize.testDummy);
+			return textSize.testDummy;
+		}();
+		testDummy.innerHTML = text;
+		testDummy.style.fontSize = fontSize;
+		var dummyWidth = (testDummy.clientWidth + 1);
+		var dummyHeight = (testDummy.clientHeight + 1);
+		return {'width' : dummyWidth, 'height' : dummyHeight};
 	}
 
 	setText(text) {
@@ -33,5 +56,4 @@ class Label extends View {
 		this.textAlign = textAlign;
 		this.view.style.textAlign = textAlign;
 	}
-
 }
